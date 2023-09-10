1a2dfd1791913f47fd4a3cf15c35ea0036c9b40b
tzwel <tzwel@int.pl>
2023-09-10 17:43:25 +0200

    colors patch

--- src/App.svelte
+++ src/App.svelte
@@ -84,13 +84,4 @@
     padding: 1rem;
   }
 
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
 </style>
