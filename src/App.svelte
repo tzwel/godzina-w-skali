4c8d789fe96b7749375efb51fd02ca8860ab9401
tzwel <tzwel@int.pl>
2023-09-10 17:28:39 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,96 @@
+<script>
+
+  const diviser = 4.1666666666666666666666666666667;
+
+  let time = {}
+
+  function calcTime() {
+    time.unix = Date.now();
+    time.date = new Date();
+    time.nowString = time.date.getHours() + ':' + pad(time.date.getMinutes());
+    time.nowPercents = (Number('' + pad(time.date.getHours()) + pad(time.date.getMinutes())) / 100) * diviser;
+    time.nowInScale = time.nowPercents / 10
+  }
+
+  calcTime()
+
+  function pad(n) { return ("0" + n).slice(-2) }
+
+</script>
+
+<main>
+
+  <h2> Teraz jest </h2>
+  <h1 class="hour big"> {time.nowString} </h1>
+
+  <section class="grid">
+    <div class="panel">
+      <h3> Godzina od 0 do 100%</h3>
+      <span class="hour">
+        {time.nowPercents.toPrecision(3)}
+      </span> <strong class="scale">/100%</strong>
+    </div>
+  
+    <div class="panel">
+      <h3> Godzina w skali od 0 do 10 </h3>
+      <span class="hour">
+        {time.nowInScale.toPrecision(2)}
+      </span> <strong class="scale">/10</strong>
+    </div>
+  </section>
+
+</main>
+
+<style>
+
+  .scale {
+    font-size: 2rem;
+  }
+
+  h2 {
+    margin-bottom: 0;
+    padding: 0;
+  }
+
+  h1, h3 {
+    margin-top: .5rem;
+  }
+
+  h3 {
+    margin-bottom: .33rem;
+  }
+
+  .hour {
+    font-size: 2rem;
+    font-weight: 700;
+    color: #ede888;
+  }
+
+  h3 {
+    font-size: 1rem;
+    font-weight: 500;
+  }
+
+  .grid {
+    display: flex;
+  }
+
+  .big {
+    font-size: 3rem;
+  }
+
+
+  .panel {
+    padding: 1rem;
+  }
+
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+</style>
